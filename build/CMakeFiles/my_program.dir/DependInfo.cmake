299b6ea896cbb9ed0a1b7f921c7c53a71aaba9da
Russel <rjmeeker3@gmail.com>
2023-12-21 01:14:37 -0500

    added cmake

---
+++ build/CMakeFiles/my_program.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/rjmeeker3/2048_DRL/GameState.c" "CMakeFiles/my_program.dir/GameState.c.o" "gcc" "CMakeFiles/my_program.dir/GameState.c.o.d"
+  "/home/rjmeeker3/2048_DRL/Queue.c" "CMakeFiles/my_program.dir/Queue.c.o" "gcc" "CMakeFiles/my_program.dir/Queue.c.o.d"
+  "/home/rjmeeker3/2048_DRL/driver.c" "CMakeFiles/my_program.dir/driver.c.o" "gcc" "CMakeFiles/my_program.dir/driver.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/my_program.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_program.dir/GameState.c.o"
+  "CMakeFiles/my_program.dir/GameState.c.o.d"
+  "CMakeFiles/my_program.dir/Queue.c.o"
+  "CMakeFiles/my_program.dir/Queue.c.o.d"
+  "CMakeFiles/my_program.dir/driver.c.o"
+  "CMakeFiles/my_program.dir/driver.c.o.d"
+  "my_program"
+  "my_program.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/my_program.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
