85d762a85cc865b887c9fd6b5d2cbb0584bc7229
Russel <rjmeeker3@gmail.com>
2023-12-20 23:36:01 -0500

    began implementing gamestate

---
+++ GameState.c
@@ -0,0 +1,46 @@
+#include "GameState.h"
+#include <stdio.h>
+
+/*  moves non-zero tiles to the left.   */
+void shift_tiles_left(int row[4]) {
+    // enqueue each element within the row
+    for (int i = 0; i < 4; i++) {
+        if (row[i] != 0) {
+            
+        }
+    }
+}
+
+/*  Merges adjacent tiles with the same value to the left.  */
+void merge_tiles_left(int row[4]) {
+
+}
+
+void apply_move(GameState *state, Action action) {
+    // save an original copy of the state for comparison
+    GameState original_state = *state;
+
+    switch (action) {
+        case MOVE_LEFT:
+            for (int i = 0; i < 4; i++) {
+                shift_tiles_left(state->board[i]);
+                merge_tiles_left(state->board[i]);
+            }
+
+        case MOVE_RIGHT:
+
+
+        case MOVE_UP:
+
+
+        case MOVE_DOWN:
+
+
+        default:
+            fprintf(stderr, "Invalid move.");
+    }
+}
+
+int is_game_over(GameState *state) {
+    
+}
---
+++ GameState.h
@@ -0,0 +1,28 @@
+#ifndef GAMESTATE_H
+#define GAMESTATE_H
+
+/*  Structure which defines a game state.
+    4x4 board represents the values of each tile at a point in time.
+    Score represents the value of the board as a single integer value.  */
+typedef struct {
+    int board[4][4];
+    int score;
+} GameState;
+
+/*  The defined set of actions which can be taken.  */
+typedef enum {
+    MOVE_LEFT,
+    MOVE_RIGHT,
+    MOVE_UP,
+    MOVE_DOWN
+} Action;
+
+/*  Applies the action to the game state.*/
+void apply_move(GameState *state, Action action);
+
+/*  Checks the game state passed for whether it is possible
+    to make another move. Returns 0 if another move is available,
+    returns 1 if there are no moves left, indicating a game over.   */
+int is_game_over(GameState *state);
+
+#endif
