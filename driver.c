44f84e1c9d6e74df6a827069cf4a2daf1b1d2963
Russel <rjmeeker3@gmail.com>
2023-12-21 01:14:22 -0500

    modified driver

--- driver.c
+++ driver.c
@@ -1,14 +1,27 @@
 #include "GameState.h"
+#include <stdio.h>
 
 int main() {
     GameState state;
 
     initializeGameState(&state);
 
-    char output[1000];
+    char output[200];
     boardToString(&state, output);
 
     printf("Initial Board:\n\n%s", output);
+
+    apply_move(&state, MOVE_LEFT);
+
+    boardToString(&state, output);
+
+    printf("New Board:\n\n%s", output);
+
+    apply_move(&state, MOVE_LEFT);
+
+    boardToString(&state, output);
+
+    printf("New Board:\n\n%s", output);
 
     return 0;
 }
