55ffe177738be60334ab477b9fa72049a38b770c
Russel <rjmeeker3@gmail.com>
2024-02-09 11:40:04 -0500

    moving away from C, switching to python

--- [DEPRECATED]code/include/NeuralNetwork.h
+++ [DEPRECATED]code/include/NeuralNetwork.h
@@ -9,6 +9,13 @@ typedef enum {
 } ActivationType;
 
 typedef struct {
+    double start_learning_rate = 0.0005;
+    double gamma = 0.9;
+    double epsilon = 0.9;
+    int memory_capacity = 6000;
+} hyperparams;
+
+typedef struct {
     int input_size;
     int output_size;
     double** weights;
@@ -21,14 +28,16 @@ typedef struct {
     Layer* layers;
 } NeuralNetwork;
 
+hyperparams->input_size;
+
 /*  handles the allocation of memory for the network. Returns a neural network
     on success, otherwise returns NULL to indicate failure.     */
 NeuralNetwork* create_network(int num_layers, int* layer_sizes, ActivationType* activations);
 /*  handles the freeing of memory for the network. */
 void destroy_network(NeuralNetwork* network);
-
+/* conducts a forward pass through the network passed to it.    */
 double* forward_pass(NeuralNetwork* network, double* input);
-
+/*  function to train the neural network.   */
 int train_network(NeuralNetwork* network, double* input, double learning_rate);
 
 #endif
